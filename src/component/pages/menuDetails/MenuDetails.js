af3fd3dcdea6e1fff90e80e8b695c5f5f981a107
mahadi215 <mahadihasanbd215@gmail.com>
2022-11-08 23:29:12 +0300

    review form added

--- src/component/pages/menuDetails/MenuDetails.js
+++ src/component/pages/menuDetails/MenuDetails.js
@@ -5,6 +5,22 @@ import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 const MenuDetails = () => {
     const {_id, name, img, des, price, ratings} = useLoaderData();
     const {user} = useContext(AuthContext);
+
+    const handleReview = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const email = user?.email;
+        const feedback = form.feedback.value;
+
+        const review = {
+            item: _id,
+            itemName:name,
+            email,
+            comment: feedback
+
+        }
+
+    }
     return (
         <div className='m-4'>
             <div className="card w-75 m-auto" >
@@ -18,10 +34,10 @@ const MenuDetails = () => {
                     <button className='btn btn-dark'>Order</button>
                     </Link>
                 </div>
-                <form  className=' w-50 sm-w-100 m-auto p-3'>
+                <form onSubmit={handleReview}  className=' w-50 sm-w-100 m-auto p-3'>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" name='email' className='form-control' defaultValue={user?.email} readOnly />
+                    <label for="exampleInputEmail1" className="form-label">Name</label>
+                    <input type="text" name='name' className='form-control' defaultValue={user?.displayName} readOnly />
 
                 </div>
                 <div className="mb-3">
