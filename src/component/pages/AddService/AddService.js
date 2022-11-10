25ef20b305079d4875b8829c101e0eb11d632f8b
mahadi215 <mahadihasanbd215@gmail.com>
2022-11-10 10:33:41 +0300

    myreview page added

--- src/component/pages/AddService/AddService.js
+++ src/component/pages/AddService/AddService.js
@@ -1,11 +1,46 @@
 import React from 'react';
 
 const AddService = () => {
+
+    const handleAddMenu = (event) =>{
+        event.preventDefault();
+        const form = event.target;
+        const name = form.ItemName.value;
+        const img = form.imgURL.value;
+        const des = form.description.value;
+        const price = form.price.value;
+        const ratings = form.ratings.value;
+
+        
+
+        const menu = {
+            name,
+            img,
+            des,
+            price,
+            ratings
+        }
+
+        fetch('http://localhost:5000/menu', {
+            method: 'post',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(menu)
+        })
+            .then(res => res.json())
+            .then(data =>{
+                if(data.acknowledged){
+                    alert('item added successfuly')
+                    form.reset();
+                }
+        })
+    }
     return (
         <div>
             <div>
                 <h3 className='text-center mt-4'>Add Service</h3>
-            <form  className=' w-75 sm-w-100 m-auto p-3'>
+            <form onSubmit={handleAddMenu}  className=' w-75 sm-w-100 m-auto p-3'>
                 <div className="mb-3">
                     <label  className="form-label">Item Name</label>
                     <input type="text" name='ItemName' className='form-control' placeholder='Enter Item Name' required />
--- src/component/pages/MyReviews/MyReviews.js
+++ src/component/pages/MyReviews/MyReviews.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../contexts/AuthProvider/AuthProvider';
 
 const MyReviews = () => {
+    const {user} = useContext(AuthContext);
+    // const {email} = user;
+    // console.log(email);
+    const [myReviews, setMyReviews] = useState([]);
+
+    useEffect(()=>{
+        fetch(`http://localhost:5000/myreviews?email=${user?.email}`)
+        .then(res => res.json())
+        .then(data => setMyReviews(data))
+    },[])
+    console.log(myReviews);
     return (
-        <div>
+        <div className='m-5'>
+            
+               {
+                 myReviews.length === 0?
+                    <h3 className='text-center'>You don't have any review</h3>
+                     : <h3 className='text-center'>You have {myReviews.length} review</h3>
+               }
+               {
+                myReviews.map(myReview => <div className='m-4 bg-light p-3'>
+                    <p><b>{myReview.itemName}</b></p>
+                    <p>{myReview.comment}</p>
+                    <button className='btn btn-dark'>Edit</button>
+                    <button className='btn btn-dark ms-3'>Delete</button>
+
+                </div>)
+               }
+            
             
         </div>
     );
